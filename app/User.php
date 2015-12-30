52d8586f107144f6dd646abc8ef428e773f67ff1
Dwi Prabowo <dwijpr@gmail.com>
2015-12-30 14:20:05 +0700

    fix softdeletes usage

--- app/User.php
+++ app/User.php
@@ -4,8 +4,11 @@ namespace ShareApp;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class User extends Authenticatable
 {
+    use SoftDeletes;
     /**
      * The attributes that are mass assignable.
      *
