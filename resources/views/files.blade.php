12d76e1dc2981d22cf208d29a87de70e442d57d2
Dwi Prabowo <dwijpr@gmail.com>
2016-01-10 13:20:24 +0700

    can go into folder

--- resources/views/files.blade.php
+++ resources/views/files.blade.php
@@ -10,22 +10,31 @@
         <thead>
             <tr>
                 <th>Name</th>
-                <th>Type</th>
                 <th>Action</th>
             </tr>
         </thead>
         <tbody>
-            @foreach($folder->folders as $_folder)
+            @if($folder->parent_id)
                 <tr>
-                    <td>{{ $_folder->name }}</td>
-                    <td>folder</td>
+                    <td 
+                        colspan="3"
+                        onclick="window.location = '/files/{{ $folder->parent_id }}'" 
+                        style="cursor: pointer;" 
+                    >
+                        ..
+                    </td>
+                </tr>
+            @endif
+            @foreach($folder->folders as $_folder)
+                <tr
+                    onclick="window.location = '/files/{{ $_folder->id }}'"
+                    style="cursor: pointer;"
+                >
                     <td>
-                        <a 
-                            href="#" 
-                            class="btn btn-info"
-                        >
-                            <i class="fa fa-edit"></i>
-                        </a>
+                        <i class="fa fa-folder"></i>
+                        {{ $_folder->name }}
+                    </td>
+                    <td>
                         {!! Form::open([
                             'url' => '/files/folder/delete/'.$_folder->id,
                             'class' => 'form-horizontal',
@@ -44,15 +53,11 @@
             @endforeach
             @foreach($folder->files as $file)
                 <tr>
-                    <td>{{ $file->name }}</td>
-                    <td>file</td>
                     <td>
-                        <a 
-                            href="#" 
-                            class="btn btn-info"
-                        >
-                            <i class="fa fa-edit"></i>
-                        </a>
+                        <i class="fa fa-file"></i>
+                        {{ $file->name }}
+                    </td>
+                    <td>
                         {!! Form::open([
                             'url' => '#',
                             'class' => 'form-horizontal',
@@ -75,6 +80,29 @@
         <h1 class="text-center">
             This folder is Empty
         </h1>
+        <div class="text-center">
+            <a 
+                href="/files/{{ $folder->parent_id }}" 
+                class="btn btn-default"
+            >
+                <i class="fa fa-sign-out"></i>
+                ..
+            </a>
+            <a
+                class="btn btn-primary"
+                href="/files/folder/new/{{ $folder->id }}" 
+            >
+                <i class="fa fa-folder"></i>
+                Create New Folder
+            </a>
+            <a
+                href="/files/upload/{{ $folder->id }}"
+                class="btn btn-info"
+            >
+                <i class="fa fa-upload"></i>
+                Upload File
+            </a>
+        </div>
     @endif
     
 @endsection
