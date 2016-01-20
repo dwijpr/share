fb971a41d106e1af8e3da2909d737e0088954c04
Dwi Prabowo <dwijpr@gmail.com>
2016-01-20 07:56:10 +0700

    image view, centered, resized

--- resources/views/files/file.blade.php
+++ resources/views/files/file.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts.base.file')
 
+
+@section('styles')
+
+    @parent
+
+    <style>
+        .img-responsive.vertically{
+            border: 1px solid rgba(0, 0, 0, .4);
+            max-height: 256px;
+            margin: 0 auto;
+        }
+        @media (min-width: 768px){
+            .img-responsive.vertically{
+                max-height: 384px;
+            }
+        }
+    </style>
+
+@endsection
+
+
 @section('navsidebar')
 
     <ul class="nav nav-sidebar">
@@ -11,6 +32,25 @@
 
     @parent
 
-    <img class="img-responsive" src="{{ $file->src }}">
+    <p class="text-center">
+        <button class="btn btn-default">
+            <i class="fa fa-sign-out"></i>
+            <span class="hidden-sm hidden-xs">Back</span>
+        </button>
+        <button class="btn btn-primary">
+            <i class="fa fa-download"></i>
+            <span class="hidden-sm hidden-xs">Download</span>
+        </button>
+        <button class="btn btn-info">
+            <i class="fa fa-picture-o"></i>
+            <span class="hidden-sm hidden-xs">Set As Profile Picture</span>
+        </button>
+    </p>
+
+    <img 
+        id="image-view" 
+        class="img-responsive vertically" 
+        src="{{ $file->src }}"
+    >
 
 @endsection
